025f6fcb1c82f970555249c810348140710bb252
wangzhichen <877062007@qq.com>
2021-09-30 15:21:18 +0800

    update) rom 判断逻辑

--- src/main/java/com/mitsuki/armory/inputmeasure/Rom.kt
+++ src/main/java/com/mitsuki/armory/inputmeasure/Rom.kt
@@ -1,11 +1,16 @@
 package com.mitsuki.armory.inputmeasure
 
 import android.os.Build
+import android.util.Log
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.lang.Exception
+import java.util.*
 
 /**
  * 难顶，其他机型日后再加吧
  */
-internal sealed class Rom {
+sealed class Rom {
     abstract fun hit(): Boolean
 
     companion object {
@@ -13,6 +18,9 @@ internal sealed class Rom {
             return when {
                 Xiaomi.hit() -> Xiaomi
                 Huawei.hit() -> Huawei
+                Oppo.hit() -> Oppo
+                Vivo.hit() -> Vivo
+                Flyme.hit() -> Flyme
                 else -> Plain
             }
         }
@@ -32,7 +40,41 @@ internal sealed class Rom {
 
     object Huawei : Rom() {
         override fun hit(): Boolean {
-            return Build.MANUFACTURER.contains("HUAWEI", true)
+            if (Build.MANUFACTURER.contains("HUAWEI", true)) return true
+            if (getProp("ro.build.version.emui").isNotEmpty()) return true
+            return false
+        }
+    }
+
+    object Oppo : Rom() {
+        override fun hit(): Boolean {
+            return getProp("ro.build.version.opporom").isNotEmpty()
+        }
+    }
+
+    object Vivo : Rom() {
+        override fun hit(): Boolean {
+            return getProp("ro.vivo.os.version").isNotEmpty()
+        }
+    }
+
+    object Flyme : Rom() {
+        override fun hit(): Boolean {
+            return Build.DISPLAY.toUpperCase(Locale.getDefault()) == "FLYME"
+        }
+    }
+
+    internal fun getProp(name: String): String {
+        try {
+            Runtime.getRuntime().exec("getprop $name").inputStream.use { inputStream ->
+                inputStream.buffered().use { bufferedInputStream ->
+                    bufferedInputStream.reader().use { reader ->
+                        return reader.readText()
+                    }
+                }
+            }
+        } catch (inn: Exception) {
+            return ""
         }
     }
 
